b31001702fd45540c11fe2258e785bf8a3468e85
Rafael Frota <rafael.frotac@gmail.com>
2024-12-05 17:13:18 -0300

    footer links

--- laravel/resources/views/components/footer.blade.php
+++ laravel/resources/views/components/footer.blade.php
@@ -4,7 +4,7 @@
             {{-- Redes Sociais --}}
             <div class="d-flex align-items-center">
                 <span class="me-2">in...</span>
-                <a href="#" class="text-white me-3"><i class="fab fa-instagram fa-lg"></i></a>
+                <a href="https://www.instagram.com/noc2k23/" class="text-white me-3"><i class="fab fa-instagram fa-lg"></i></a>
                 <a href="#" class="text-white me-3"><i class="fab fa-whatsapp fa-lg"></i></a>
                 <a href="#" class="text-white me-3"><i class="fab fa-tiktok fa-lg"></i></a>
                 <a href="#" class="text-white"><i class="fab fa-twitter fa-lg"></i></a>
@@ -12,10 +12,10 @@
 
             {{-- Links do Rodapé --}}
             <div class="d-flex">
-                <a href="#" class="text-white text-decoration-none mx-3">Contato</a>
-                <a href="#" class="text-white text-decoration-none mx-3">Trocas & Devoluções</a>
-                <a href="#" class="text-white text-decoration-none mx-3">Envio & Pagamento</a>
-                <a href="#" class="text-white text-decoration-none mx-3">Termos de Uso</a>
+                <a href="{{ route('contact') }}" class="text-white text-decoration-none mx-3">Contato</a>
+                <a href="{{ route('returns') }}" class="text-white text-decoration-none mx-3">Trocas & Devoluções</a>
+                <a href="{{ route('shipping') }}" class="text-white text-decoration-none mx-3">Envio & Pagamento</a>
+                <a href="{{ route('terms') }}" class="text-white text-decoration-none mx-3">Termos de Uso</a>
             </div>
         </div>
     </div>
